969d6d55d2eae57dd3575ce7421315dd0b056a0e
Alfonso Feria <alfonso.feria@konfio.mx>
2020-07-01 00:08:48 -0500

    Added Sidebar Toggle

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,11 +3,11 @@ import Links from "../constants/links"
 import SocialLinks from "../constants/socialLinks"
 import { FaTimes } from "react-icons/fa"
 import { Link } from "gatsby"
-const Sidebar = () => {
-  const isOpen = false
+
+const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
     <aside className={`sidebar ${isOpen ? "show-sidebar" : ""}`}>
-      <button className="close-btn">
+      <button className="close-btn" onClick={toggleSidebar}>
         <FaTimes />
       </button>
       <div className="side-container">

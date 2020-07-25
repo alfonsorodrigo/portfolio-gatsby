64b12d10831fc4fa06afbc566659b5aca469239b
Alfonso Feria <alfonso.feria@konfio.mx>
2020-07-25 16:19:01 -0500

    Added Formspree

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,23 +7,28 @@ const contact = () => {
       <section className="contact-page">
         <article className="contact-form">
           <h3>Contáctame</h3>
-          <form>
+          <form action="https://formspree.io/xbjzqpjk" method="POST">
             <div className="form-group">
               <input
                 type="text"
                 placeholder="Nombre"
                 className="form-control"
+                name="name"
+                required
               />
               <input
                 type="email"
                 placeholder="email"
                 className="form-control"
+                name="email"
+                required
               />
               <textarea
                 name="message"
                 rows="5"
                 placeholder="message"
                 className="form-control"
+                required
               ></textarea>
             </div>
             <button type="submit" className="submit-btn btn">

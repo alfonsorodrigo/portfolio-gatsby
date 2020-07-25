7c841917d9e77d53bd7ba5f171c96719746555a7
Alfonso Feria <alfonso.feria@konfio.mx>
2020-07-25 15:58:15 -0500

    Added Contact Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,38 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return (
+    <Layout>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>Contáctame</h3>
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Nombre"
+                className="form-control"
+              />
+              <input
+                type="email"
+                placeholder="email"
+                className="form-control"
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="message"
+                className="form-control"
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              Enviar
+            </button>
+          </form>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default contact
